fb493e0bd1a255212b1df25248fe1922d62af199
Matthias Mehrpuyan <mmehrpuyan@gmail.com>
2016-10-19 18:44:52 -0500

    Final

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -10,7 +10,20 @@ class MeteorologistController < ApplicationController
     @street_address = params[:user_street_address]
     @street_address_without_spaces = URI.encode(@street_address)
 
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address_without_spaces
     # ==========================================================================
+    parsed_data = JSON.parse(open(url).read)
+
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"].to_s
+
+    @longitude =  parsed_data["results"][0]["geometry"]["location"]["lng"].to_s
+
+    @coords = @latitude + "," + @longitude
+    URI.encode(@coords)
+
+    url = "https://api.darksky.net/forecast/52e28620083922a5ee9201e442356b09/" + @coords
+
+    parsed_data = JSON.parse(open(url).read)
     # Your code goes below.
     # The street address the user input is in the variable @street_address.
     # A URL-safe version of the street address, with spaces and other illegal
@@ -19,15 +32,15 @@ class MeteorologistController < ApplicationController
 
 
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = parsed_data["currently"]["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
     render("meteorologist/street_to_weather.html.erb")
   end
